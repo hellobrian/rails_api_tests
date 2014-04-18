388c92b4d4721ea03ff312f0ac91abb54ec69731
Brian Han <brianhan87usa@gmail.com>
2014-04-17 21:47:59 -0400

    Add test for prohibiting nil description values

--- test/integration/creating_goals_test.rb
+++ test/integration/creating_goals_test.rb
@@ -5,7 +5,6 @@ class CreatingGoalsTest < ActionDispatch::IntegrationTest
   setup { 
     host! 'api.example.com' 
     @goal = build(:goal)
-    @nil_goal = build(:goal, motivation: nil)
   }
 
   test 'creates goals' do 
@@ -20,8 +19,18 @@ class CreatingGoalsTest < ActionDispatch::IntegrationTest
     assert_equal response.location, api_goal_url(goal[:id])
   end
 
+  test 'does not create goals with nil description' do 
+    @nil_goal = build(:goal, description: nil)
+    post '/goals', 
+      @nil_goal.to_json, 
+      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+
+    assert_equal 422, response.status
+    assert_equal Mime::JSON, response.content_type
+  end
+
   test 'does not create goals with nil motivation' do 
-    
+    @nil_goal = build(:goal, motivation: nil)
     post '/goals', 
       @nil_goal.to_json,
       { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
@@ -29,4 +38,6 @@ class CreatingGoalsTest < ActionDispatch::IntegrationTest
     assert_equal 422, response.status
     assert_equal Mime::JSON, response.content_type
   end
+
+
 end
